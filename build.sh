21385f30ecf9673689c2d083c33480125e5e00c3
chenxi-Eumenides <1742041477@qq.com>
2025-03-11 23:31:04 +0800

    update: update hugo version to 0.143 and change folder structure at 2025-03-11_23:31:04

--- build.sh
+++ build.sh
@@ -5,16 +5,25 @@ setup() {
     desc="cheni-zqs的blog网站，用hugo搭建，主题为stack。"
 }
 
+main() {
+    [[ -z $1 ]] && read -p "input commit info: " input && { [ -z $input ] && echo "no input." && return 1; }
+    [[ -z $input ]] && input=$*
+    update_git $input
+    rm -r public/*
+    rm -r public-github/
+}
+
 build_draft() {
     update_git $*
-    delete_old_public
-    hugo --buildDrafts
+    rm -r ./public-github
+    hugo --config hugo-github.yaml --buildDrafts
 }
 
 build() {
     update_git $*
-    delete_old_public
-    hugo
+    rm -r public
+    hugo --config hugo-local.yaml
+    push_git
 }
 
 update_git() {
@@ -22,14 +31,17 @@ update_git() {
     if [[ -z $1 ]] ; then
         content="update: Auto build by runsh at $(date +%F_%T)"
     else
-        content="$*"
+        content="$* at $(date +%F_%T)"
     fi
     git commit -m "${content}"
 }
 
-delete_old_public(){
-    rm -r ./public/
+push_git() {
+    cd public-github
+    git push github
 }
+
+#delete
 
 init() {
     cd $(dirname $0)
@@ -46,11 +58,8 @@ help() {
 
 init
 case $1 in
-  "draft")
-    build_draft ${@:2}
-    ;;
   "build")
-    build ${@:2}
+    main ${@:2}
     ;;
   *)
     help
